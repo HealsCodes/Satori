522daa331443113bdfdc7be6a35bd59eea7c9930
René Köcher <shirk@bitspin.org>
2010-09-26 23:50:24 +0200

    Rewrite from scratch. - thread-safe rewrite - fixed SQLAchemy models.

--- satori/book_keeper.py
+++ satori/book_keeper.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #
-#  userdb.py
+#  book_keeper.py
 #
 # Copyright (c) 2010 René Köcher <shirk@bitspin.org>
 # All rights reserved.
@@ -27,137 +27,340 @@
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+import thread, threading
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData, Column, Table, ForeignKey
 from sqlalchemy import Integer, String
+from sqlalchemy.orm import backref, relationship, sessionmaker, scoped_session
+from sqlalchemy.orm import mapper as sqla_mapper
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import backref, mapper, relation, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 
-Base = declarative_base()
+def _session_mapper(cls, scoped_session_):
+    """
+    support for scoped_session aware mapper class as described in
+    http://www.sqlalchemy.org/trac/wiki/UsageRecipes/SessionAwareMapper
+    """
+    def mapper(cls, *args, **kwargs):
+        cls.query = scoped_session_.query_property()
+        return sqla_mapper(cls, *args, **kwargs)
+    return mapper
 
-# ----
+_Session = scoped_session(sessionmaker())
+_Base = declarative_base()
+_mapper = _session_mapper(_Base, _Session)
+_Base.mapper = _mapper
 
-class User(Base):
-    __tablename__ = 'users'
+class Account(_Base):
+    __tablename__ = 'account'
     
-    id = Column(Integer, primary_key=True)
-    jid = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    service_id = Column(Integer, ForeignKey('service.id'), primary_key=True)
+    
+    auth_key = Column(String, nullable=False)
+    auth_secret = Column(String, nullable=False)
+    status = Column(String)
+    
+
+    def __init__(self, user, service, auth_key='', auth_secret=''):
+        self.user_id = user.id_
+        self.service_id = service.id_
+        self.user = user
+        self.service = service
+        self.auth_key = auth_key
+        self.auth_secret = auth_secret
+        self.status = ''
+    
+    def __repr__(self):
+        return '<Account(user_id={0}, service_id={1}, auth_key="{2}", auth_secret="{3}", status="{4}")>'.format(
+            self.user_id, self.service_id, ('x' * 10) + self.auth_key[-4:],
+            ('x' * 10) + self.auth_secret[-4:], self.status)
+
+
+class Service(_Base):
+    __tablename__ = 'service'
+    
+    id_ = Column('id', Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    type_id = Column(Integer, ForeignKey('service_type.id'), nullable=False)
+    
+    accounts = relationship(Account, backref='service', cascade='all, delete, delete-orphan')
+    
+    def __init__(self, name, service_type):
+        self.name = name
+        self.type_id = service_type.id_
+        self.type_ = service_type
+    
+    def __repr__(self):
+        return '<Service(id={0}, name="{1}", type_id={2})>'.format(
+            self.id_, self.name, self.type_id)
+
+class ServiceType(_Base):
+    __tablename__ = 'service_type'
+
+    id_ = Column('id', Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+    services = relationship(Service, backref='type_', cascade='all, delete, delete-orphan')
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<ServiceType(id={0}, name="{1}")>'.format(self.id_, self.name)
+
+class User(_Base):
+    __tablename__ = 'user'
+
+    id_ = Column('id', Integer, primary_key=True)
+    jid = Column(String, nullable=False, unique=True)
+
+    accounts = relationship(Account, backref='user', cascade='all, delete, delete-orphan')
     
     def __init__(self, jid):
         self.jid = jid
-    
-    def __repr__(self):
-        return '<User(\'{0}\')>'.format(self.jid)
 
-class AccountType(Base):
-    __tablename__ = 'account_types'
-    
-    id = Column(Integer, nullable=False, default=0, autoincrement=True)
-    name = Column(String, nullable=False, primary_key=True)
-    tag = Column(String, nullable=False, primary_key=True)
-    
-    def __init__(self, name, tag):
-        self.name = name
-        self.tag = tag
-        
     def __repr__(self):
-        return '<AccountType(\'{0}\', \'{1}\')>'.format(self.name, self.tag)
+        return '<User(id={0}, jid="{1}")>'.format(self.id_, self.jid)
 
-class AccountData(Base):
-    __tablename__ = 'account_data'
-    
-    id = Column(Integer, primary_key=True)
-    key = Column(String, nullable=True) # user for basic auth, key for oAuth
-    secret = Column(String, nullable=True)
-    state = Column(String, nullable=False, default='0')
-    user_id = Column(Integer, ForeignKey('users.id'))
-    type_id = Column(Integer, ForeignKey('account_types.id'))
-    
-    user = relation(User, backref=backref('accounts', order_by=id), 
-                                          cascade='all, delete')
-    atype = relation(AccountType, cascade='all, delete')
-
-    def __init__(self, key, secret):
-        self.key = key
-        self.secret = secret
-    
-    def __repr__(self):
-        return '<AccountData(\'xxxx{0}\', \'xxxx{1}\')>'.format(self.key[-4:], self.secret[-4:])
 
 class BookKeeper(object):
     def __init__(self, dbpath):
-        self._engine = create_engine('sqlite:///%s' % dbpath)
-        self._metadata = Base.metadata
-        self._session = sessionmaker(bind=self._engine)()
-        self._metadata.create_all(self._engine)
+        self._engine = create_engine('sqlite:///{0}'.format(dbpath))
+        _Session.configure(bind=self._engine)
+        _Base.metadata.create_all(self._engine)
+        self._sessions = {}
 
-    def __del__(self):
-        if self._session.dirty or self._session.new:
-            self._session.commit()
-        self._session.close()
+    def _local_session(self):
+        if not thread.get_ident() in self._sessions:
+            self._sessions[thread.get_ident()] = _Session()
+        return self._sessions[thread.get_ident()]
 
-    def update_records(self, obj=None):
-        if obj:
-            self._session.add(obj)
+    def reflect_services(self, config):
+        if not getattr(config, 'services', None):
+            return
         
-        if self._session.new or self._session.dirty:
-            self._session.commit()
-
+        try:
+            session = self._local_session()
+            # step 1, remove all services _not_ in the config file
+            # this will cascade down to the accounts using them
+            for service in session.query(Service).all():
+                if not service.name in [x['tag'] for x in config.services]:
+                    print '* removing obsolete {0}'.format(service)
+                    session.delete(service)
+            
+            # step 2, add new service types (if any)
+            known_service_types = [x[0] for x in session.query(ServiceType.name).all()]
+            for service_type in [x['type'] for x in config.services if not x['type'] in known_service_types]:
+                session.add(ServiceType(service_type))
+                print '* creating new service_type {0}'.format(service_type)
+            session.commit()
+            
+            # step 3, add new services (if any)
+            known_services = [x[0] for x in session.query(Service.name).all()]
+            for service in [x for x in config.services if not x['tag'] in known_services]:
+                service_type = session.query(ServiceType).filter(ServiceType.name == service['type']).first()
+                session.add(Service(service['tag'], service_type))
+                print '* creating new service {0} - {1}'.format(service['tag'], service_type)
+            
+            # step 4, remove service types no longer in use
+            for service_type in session.query(ServiceType):
+                if not len(service_type.services):
+                    print '* removing orphaned {0}'.format(service_type)
+                    
+            # step 5, remove orphaned users
+            for user in session.query(User):
+                if not len(user.accounts):
+                    print '* removing orphaned {0}'.format(user)
+            session.commit()
+            return True
+        except SQLAlchemyError as err:
+            print '\n***'
+            print 'Something went wrong - attempting a rollback!'
+            print 'Error was: {0}'.format(err)
+            print '***\n'
+            session.rollback()
+            return False
+        
+    def account(self, jid=None, service=None, create=False):
+        sql = self._local_session().query(Account)
+        if service:
+            sql = sql.join(Service)
+            sql = sql.filter(Account.service_id == Service.id_)
+            sql = sql.filter(Service.name == service)
+        if jid:
+            sql = sql.join(User)
+            sql = sql.filter(Account.user_id == User.id_)
+            sql = sql.filter(User.jid == jid)
+        
+        res = sql.all()
+        if create and jid and service and not res:
+            user = self._local_session().query(User).filter(User.jid == jid).first()
+            service_ = self._local_session().query(Service).filter(Service.name == service).first()
+            res = [Account(user, service_)]
+            self.commit(res[0])
+        
+        return res
+    
+    def user(self, jid=None, create=False):
+        sql = self._local_session().query(User)
+        if jid:
+            sql = sql.filter(User.jid == jid)
+        
+        res = sql.all()
+        if create and not res:
+            res = [User(jid),]
+            self.commit(res[0])
+            
+        return res
+    
     def remove(self, obj):
-        self._session.delete(obj)
-        self._session.commit()
-
-    def user(self, jid, create=False):
-        sql = self._session.query(User).filter(User.jid == jid)
-        if sql.first():
-            return sql.first()
-        
-        if not create:
-            return None
-        
-        user = User(jid)
-        self._session.add(user)
-        self._session.commit()
-        
-        return user
+        session = self._local_session()
+        if obj:
+            session.delete(obj)
+        session.commit()
     
-    def account_type(self, name, tag):
-        sql = self._session.query(AccountType)
-        sql = sql.filter(AccountType.name == name)
-        sql = sql.filter(AccountType.tag == tag)
-        
-        if sql.first():
-            return sql.first()
-        
-        account_type = AccountType(name, tag)
-        self._session.add(account_type)
-        self._session.commit()
-        
-        return account_type
+    def commit(self, obj):
+        session = self._local_session()
+        if obj:
+            session.add(obj)
+        session.commit()
     
-    def account_data(self, jid, tag, key, secret, create=False):
-        user  = self._session.query(User).filter(User.jid == jid).first()
-        atype = self._session.query(AccountType).filter(AccountType.tag == tag).first()
+    def release(self):
+        if thread.get_ident() in self._sessions:
+            self._sessions[thread.get_ident()].close()
+            del self._sessions[thread.get_ident()]
+    
+    def do_tests(self):
+        import time, random, thread, threading
         
-        if not user or not atype:
-            return None
+        def _thread_updater(bookkeeper, jid):
+                for i in range(0, 5):
+                    account = bookkeeper.account(jid=jid)[0]
+                    ident = thread.get_ident()
+                    print '- thread {0}: updating data..'.format(ident)
+                    print '- thread {0}: before: {1}'.format(ident, account)
+                    account.status = '{0}:+{1}'.format(ident, i)
+                    bookkeeper.commit(account)
+                    print '- thread {0}: after : {1}'.format(ident, account)
+                    time.sleep(random.randint(1, 10))
+                print '- thread {0}: terminating'.format(thread.get_ident())
+        
+        users = []
+        service_ids = []
+        services = []
+        accounts = []
+        
+        print '* Creating basic data..'
+        try:
+            session = self._local_session()
+            users += self.user('john@example.org', create=True)
+            users += self.user('jane@example.org', create=True)
+            service_ids.append(ServiceType('twitter_oAuth'))
+            service_ids.append(ServiceType('twitter_BasicAuth'))
+            services.append(Service('twitter', service_ids[0]))
+            services.append(Service('identi.ca', service_ids[1]))
+            session.add_all(service_ids)
+            session.add_all(services)
+            session.commit()
+
+            accounts += self.account(users[0].jid, services[0].name, create=True)
+            accounts += self.account(users[0].jid, services[1].name, create=True)
+            accounts += self.account(users[1].jid, services[0].name, create=True)
+            accounts += self.account(users[1].jid, services[1].name, create=True)
             
-        sql = self._session.query(AccountData)
-        sql = sql.filter(AccountData.user == user)
-        sql = sql.filter(AccountData.atype == atype)
+            session.add_all(accounts)
+            session.commit()
+
         
-        if sql.first():
-            return sql.first()
+            print '* Recursive printout for users:'
+            for user in users:
+                print '- {0}'.format(user)
+                for account in user.accounts:
+                    print '    --- {0}'.format(account)
+                    print '        --- {0}'.format(account.service)
+                    print '            --- {0}'.format(account.service.type_)
+                print ''
+        
+                print '* Recursive printout for service_ids:'
+                for service_id in service_ids:
+                    print '- {0}'.format(service_id)
+                    for service in service_id.services:
+                        print '    --- {0}'.format(service)
+                        for account in service.accounts:
+                            print '        --- {0}'.format(account)
+                    print ''
             
-        if not create or (create and (key is None or secret is None)):
-            return None
+            print '* Attempting to remove a single account..'
+            del users[0].accounts[0]
+            session.commit()
         
-        account_data = AccountData(key, secret)
-        account_data.user = user
-        account_data.atype = atype
-        self._session.add(account_data)
-        self._session.commit()
+            print '- Nr. of Accounts for users[0]: {0}'.format(len(users[0].accounts))
+            if len(users[0].accounts) != 1:
+                print '\n***'
+                print 'Somathing went wrong, I expected only one account!'
+                print '***\n'
+                return False
+            else:
+                print ''
         
-        return account_data
+            print '* Attempting a cascaded delete of service_ids[0]..'
+            
+            session.delete(service_ids[0])
+            session.commit()
+            res = session.query(Service).filter(Service.type_ == service_ids[0]).all()
+            if res:
+                print '\n***'
+                print 'Something went wrong, I expected no remaining Services!'
+                print 'Got: {0}'.format(res)
+                print '***\n'
+                return False
+            else:
+                print '- no Services left referencing this service_type'
+                
+            res = session.query(Account).filter(Account.service == services[0]).all()
+            if res:
+                print '\n***'
+                print 'Something went wrong, I expected no remaining Accounts!'
+                print 'Got: {0}'.format(res)
+                print '***\n'
+                return False
+            else:
+                print '- no Accounts left referencing this service_type'
+                print '- OK\n'
+                
+            print '* Attempting a cascaded delete of all users[1]..'
+            session.delete(users[1])
+            session.commit()
+            res = session.query(Account).filter(Account.user == users[1]).all()
+            if res:
+                print '\n***'
+                print 'Something went wrong, I expected no remaining Accounts!'
+                print 'Got: {0}'.format(res)
+                print '***\n'
+                return False
+            else:
+                print '- no Accounts left referencing this user'
+                print '- OK\n'
+            
+            print '* Attempting a multi-threaded data update..'
+            threading.Thread(target=_thread_updater, args=(self, users[0].jid,)).start()
+            threading.Thread(target=_thread_updater, args=(self, users[0].jid,)).start()
+            
+            while threading.active_count() > 1:
+                time.sleep(1)
+            print '- OK\n'
+            
+        except SQLAlchemyError as err:
+            print '\n***'
+            print 'Something went wrong, please make sure "test.db" doesn\'t already exist.'
+            print 'Error was: {0}'.format(err)
+            print '***\n'
+            return False
+        
+        print '--- Looks good :)'
         
 
+if __name__ == '__main__':
+    bookkeeper = BookKeeper('test.db')
+    bookkeeper.do_tests()
